3bc0bc3ec903cfe5041e6db42714092f70ecf49a
haoxingwei <haoxingwei@eastmoney.com>
2022-12-30 17:09:18 +0800

    feat: cors and json parse

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import SERVER_CONFIG from '../configs/server.config.json';
 import { HI_MSG } from './utils/constant';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/hi', (req, res) => {
+/* cors */
+app.use(
+  cors({
+    origin: '*'
+  })
+);
+/* json parse */
+app.use(express.json());
+
+app.post('/hi', (req, res) => {
+  console.log(req.body);
+  console.log(req.body.name);
   res.send(HI_MSG);
 });
 
